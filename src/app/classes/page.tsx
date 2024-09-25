fbfcb215149b1aa1cc7909b586873bb8940c9624
purpl3bot <mechi.farias@gmail.com>
2024-09-26 01:19:01 +0200

    Update page.tsx

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -8,10 +8,24 @@ export default function Classes() {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
         <div>
           <p className="mb-4">
-            Our classes are designed to be interactive and fun. We use a variety of methods to help you learn Spanish quickly and effectively.
+            H O W W E T E A C H
+Our methodology is based on the communicative approach, which means that you will
+learn to use grammatical and lexical structures in practical, real-life situations. The
+aim is to make you feel comfortable and confident. In addition, in each class you will
+have one of us as your teacher, as we alternate sessions to offer you different teaching
+styles. This will help you gain a broader exposure to different accents, educational
+resources, perspectives and ways of approaching the language.
           </p>
           <p>
-            Whether you're a beginner or looking to improve your existing skills, we have a class that's right for you.
+           O U R C L A S S E S
+We offer classes for beginners (A1-A2), intermediates (B1-B2), and advanced
+learners (C1-C2).
+Individual classes, small group classes, face-to-face and online classes.
+Conversation classes.
+Special classes for travelers (learn everything you need to handle yourself on a
+trip in Spanish and be an independent traveler!)
+Flexible schedules according to your availability.
+All materials included.
           </p>
         </div>
         <div className="flex justify-center items-center">
