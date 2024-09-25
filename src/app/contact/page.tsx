8274a1c0f75e5774e84ea975c60222c68ee1010f
purpl3bot <mechi.farias@gmail.com>
2024-09-26 01:42:47 +0200

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ export default function Contact() {
       <h1 className="font-hand text-3xl font-bold mb-8 text-center">Contact Us</h1>
       <div className="max-w-md mx-auto">
         <p className="mb-4">
-          We'd love to hear from you! If you have any questions about our classes or would like to schedule a trial lesson, please don't hesitate to get in touch by email or over the <a href="https://docs.google.com/forms/d/e/1FAIpQLSfw4pjFjkztGIKiBPw4_-Cra5Oy7abA4kuu9bq1YMrqWELdKw/viewform" class="text-orange-600 underline dark:text-blue-500 hover:no-underline">form</a>.
+          We'd love to hear from you! If you have any questions about our classes or would like to schedule a trial lesson, please don't hesitate to get in touch by email or over the <a href="https://docs.google.com/forms/d/e/1FAIpQLSfw4pjFjkztGIKiBPw4_-Cra5Oy7abA4kuu9bq1YMrqWELdKw/viewform" className="text-orange-600 underline dark:text-blue-500 hover:no-underline">form</a>.
         </p>
         <div className="bg-yellow-100 p-4 rounded-lg">
           <h2 className="text-xl font-bold mb-2">Contact Information</h2>
