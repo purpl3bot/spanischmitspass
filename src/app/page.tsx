2f7e83330374e15ca14c934661739865ff5d3307
purpl3bot <mechi.farias@gmail.com>
2024-09-26 01:34:42 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="w-120 h-120 relative mb-8">
+      <div className="w-64 h-64 relative mb-8">
         <Image
           src="/disenio.jpg"
           alt="Animated GIF"
