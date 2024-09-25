5f9e95bbc33062bcb9c9ffe3ece033b00f4078e7
purpl3bot <mechi.farias@gmail.com>
2024-09-25 17:15:37 +0200

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="w-64 h-64 relative mb-8">
+        <Image
+          src="/images/placeholder.gif"
+          alt="Animated GIF"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <p className="text-center text-lg max-w-md">
+        Welcome to Spanisch mit Spass! Learn Spanish in a fun and engaging way with our experienced teachers in Berlin.
+      </p>
+    </div>
+  )
+}
